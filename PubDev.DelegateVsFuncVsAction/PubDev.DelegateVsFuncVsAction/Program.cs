8d03a21edc3a6c29df57e0c3fb2ea9eb99384a55
Humberto Rodrigues <humberto.fcamara@superdigital.com.br>
2021-07-03 23:20:45 -0300

    primeiro comit, com itens referentes a Delegates

---
+++ PubDev.DelegateVsFuncVsAction/PubDev.DelegateVsFuncVsAction/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using static PubDev.DelegateVsFuncVsAction.Pagamento;
+
+namespace PubDev.DelegateVsFuncVsAction
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+
+            PagamentoProcessado pagamentoProcessado = (itemId) =>
+            {
+                Console.WriteLine($"Item - {itemId} processado!");
+            };
+
+            pagamento.Processar(pagamentoProcessado);
+
+            pagamento.Calcular(itemId =>
+            {
+                Console.WriteLine($"Item - {itemId} calculado!");
+
+                return Math.Pow(itemId, 0.15d);
+            });
+        }
+    }
+
+    public class Pagamento
+    {
+        public delegate void PagamentoProcessado(int itemId);
+
+        public void Processar(PagamentoProcessado pagamentoProcessado)
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                //Simulando pagamento
+                Thread.Sleep(1000);
+                //Status Processamento
+                pagamentoProcessado.Invoke(i + 1);
+            }
+        }
+
+        public delegate double PagamentoCalculado(int itemId);
+
+        public void Calcular(PagamentoCalculado pagamentoCalculado)
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                //Simulando pagamento
+                Thread.Sleep(1000);
+
+                //Status Calculo
+                int pagamentoId = i + 1;
+
+                double valor = pagamentoCalculado.Invoke(pagamentoId);
+
+                Console.WriteLine($"Valor calculado para pagamento {pagamentoId} = {valor:C2}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Lesson-DotNet-Delegate
 Projeto que tem como principal objetivo demonstrar quais são as diferenças entre Delegates, Funcs e Actions no C#
+
+References:
+ 
+ - [Delegates](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/delegates)
+  
+ Aproveite e se inscreva no nosso canal no youtube : [PubDev](https://www.youtube.com/pubdev)
+
+ e nos siga nas redes sociais
+
+ - [instagram](https://www.instagram.com/pub_dev)
+ - [telegram](https://t.me/pub_dev)
+ - [loja](https://loja.pubdev.com.br)
+1berto
+ - [instagram](https://www.instagram.com/1bberto)
+ - [facebook](https://web.facebook.com/1bberto)
+
+Naga
+ - [instagram](https://www.instagram.com/rafakenji23)
+ - [facebook](https://web.facebook.com/rafakenji.japa)
